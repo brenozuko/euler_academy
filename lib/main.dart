5bd5f7cadcb535aee1e361291fcf70ad374d6c46
JeanLuca999 <66375859+JeanLuca999@users.noreply.github.com>
2021-09-16 11:22:05 -0300

    Tela inicial

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:euler_academy/pages/login_page.dart';
+import 'pages/welcome_page.dart';
 
 void main() {
   // ignore: prefer_const_constructors
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    title: 'IMC',
-    home: LoginPage(),
+    title: 'Euler Academy',
+    home: WelcomePage(),
   ));
 }
---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,53 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return (Column(children: [
+      Column(children: [
+        Image.asset('assets/euler_logo.png'),
+        Image.asset('assets/telaInicial.png'),
+        Container(
+            child: Text('Aprenda o que quiser, não importa a hora ou lugar!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xff274378),
+                  decoration: TextDecoration.none,
+                ))),
+        Container(
+            margin: const EdgeInsets.only(bottom: 20.0, top: 20.0),
+            child: Text('A Euler Academy está disponível em diversos idiomas.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w100,
+                  color: Color(0xff000000),
+                  decoration: TextDecoration.none,
+                ))),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.only(top: 20, bottom: 20, right: 30, left: 30),
+            primary: Color(0xff1865f2),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            elevation: 15,
+            shadowColor: Colors.black,
+          ),
+          child: Text('Comece aqui',
+              style: TextStyle(
+                color: Colors.white,
+              )),
+          onPressed: () {},
+        ),
+      ])
+    ]));
+  }
+}
