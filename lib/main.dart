12f84a940b6608d7f3d6bdb8b5257cec2c2f7d4e
Breno Zukowski <brenozukotech@gmail.com>
2021-09-16 07:59:53 -0300

    Started login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:euler_academy/pages/login_page.dart';
 
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'IMC',
+    home: LoginPage(),
+  ));
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(
+          top: 60,
+          left: 40,
+          right: 40,
+        ),
+        color: Colors.white,
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              width: 128,
+            )
+
+          ]
+        ),
+      ),
+    );
+  }
+}
