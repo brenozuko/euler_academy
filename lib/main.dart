82875b3e7afad10f688fa20e7c3f2c9201cf13df
Breno Zukowski <brenozukotech@gmail.com>
2021-09-16 09:32:53 -0300

    Added appbar to login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:euler_academy/pages/login_page.dart';
 
 void main() {
+  // ignore: prefer_const_constructors
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     title: 'IMC',
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,11 +1,25 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Faça login ou cadastre-se',
+            style: TextStyle(
+              color: Colors.white,
+            )),
+        backgroundColor: Color(0x274378),
+      ),
       body: Container(
         padding: EdgeInsets.only(
           top: 60,
@@ -13,14 +27,44 @@ class LoginPage extends StatelessWidget {
           right: 40,
         ),
         color: Colors.white,
-        child: ListView(
-          children: <Widget>[
-            SizedBox(
-              width: 128,
-            )
-
-          ]
-        ),
+        child: ListView(children: <Widget>[
+          SizedBox(
+            child: Image.asset("assets/euler_logo.png"),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          TextFormField(
+            // autofocus: true,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              labelText: "E-mail",
+              labelStyle: TextStyle(
+                color: Colors.black38,
+                fontWeight: FontWeight.w400,
+                fontSize: 20,
+              ),
+            ),
+            style: TextStyle(fontSize: 20),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          TextFormField(
+            // autofocus: true,
+            keyboardType: TextInputType.text,
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: "Senha",
+              labelStyle: TextStyle(
+                color: Colors.black38,
+                fontWeight: FontWeight.w400,
+                fontSize: 20,
+              ),
+            ),
+            style: TextStyle(fontSize: 20),
+          ),
+        ]),
       ),
     );
   }
