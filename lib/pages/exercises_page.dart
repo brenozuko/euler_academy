400b11df1f021df06609b37ba1f7114cab2f4601
Breno Zukowski <brenozukotech@gmail.com>
2021-10-20 11:45:56 -0300

    Added search to exercises list

--- lib/pages/exercises_page.dart
+++ lib/pages/exercises_page.dart
@@ -40,63 +40,93 @@ class _ExercisesPageState extends State<ExercisesPage> {
         backgroundColor: Color(0xfff4f4f4),
         iconTheme: IconThemeData(color: Color(0xff274378)),
       ),
-      body: Container(
-        padding: EdgeInsets.all(20),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 4,
+            child: Container(
+              padding: EdgeInsets.all(20),
 
-        //
-        // ListView
-        //
-        child: ListView.builder(
-          //quantidade de elementos da lista
-          itemCount: lista.length,
+              //
+              // ListView
+              //
+              child: ListView.builder(
+                //quantidade de elementos da lista
+                itemCount: lista.length,
 
-          //alterar a orientação (vertical/horizontal)
-          //scrollDirection: Axis.horizontal,
+                //alterar a orientação (vertical/horizontal)
+                //scrollDirection: Axis.horizontal,
 
-          //definir a aparência dos elementos
-          itemBuilder: (context, index) {
-            return Container(
-              margin: EdgeInsets.only(top: 20),
-              child: ListTile(
-                //leading: Icon(Icons.arrow_right),
-                title: Text(
-                  lista[index],
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Color(0xff274378),
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                trailing: ElevatedButton(
-                  onPressed: () {},
-                  child: const Text(
-                    'Iniciar',
-                    style: TextStyle(
-                      fontFamily: 'Lato',
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
+                //definir a aparência dos elementos
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: EdgeInsets.only(top: 20),
+                    child: ListTile(
+                      //leading: Icon(Icons.arrow_right),
+                      title: Text(
+                        lista[index],
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Color(0xff274378),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      trailing: ElevatedButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Iniciar',
+                          style: TextStyle(
+                            fontFamily: 'Lato',
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
-                ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 0,
+                          blurRadius: 5,
+                          offset: Offset(0, 0), // changes position of shadow
+                        )
+                      ],
+                    ),
+                  );
+                },
               ),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(10),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 0,
-                    blurRadius: 5,
-                    offset: Offset(0, 0), // changes position of shadow
-                  )
-                ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: SizedBox(
+              width: 300,
+              child: Container(
+                margin: EdgeInsets.only(top: 40, bottom: 40),
+                child: Material(
+                    elevation: 20.0,
+                    shadowColor: Colors.black45,
+                    borderRadius: BorderRadius.circular(20),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.search),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20.0)),
+                          filled: true,
+                          hintStyle: TextStyle(color: Color(0xffABA4A4)),
+                          hintText: "Pesquisar por algo específico...",
+                          fillColor: Color(0xffffffff)),
+                    )),
               ),
-            );
-          },
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
